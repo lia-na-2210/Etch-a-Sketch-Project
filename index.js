22484c8cc594b338c373f8eb4242ddf8c5dec2e3
lia-na-2210 <naneko.bl@gmail.com>
2023-01-02 21:24:44 -0600

    input and backgroun color change done

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const mainContainer = document.getElementById('container');
 
-function createGrid (numDivs) {
-    for (i=0; i < numDivs; i++) {
+function createGrid (size) {
+    mainContainer.style.gridTemplateColumns = `repeat(${size}, 1fr`;
+    mainContainer.style.gridTemplateRows = `repeat(${size}, 1fr`;
+
+    let squares = mainContainer.querySelectorAll('div');
+    squares.forEach((div)=> div.remove());
+
+    let amount = size * size;
+
+    for (i=0; i < amount; i++) {
         const cells = document.createElement('div');
         cells.setAttribute('class', 'cell');
         cells.innerText = '';
 
         mainContainer.appendChild(cells);
-        cells.addEventListener('mouseover', mouseOver(cells));
+        cells.addEventListener('mouseover', () => {
+            cells.style.backgroundColor = 'black';
+        });
     }
 
     
 }
 
-function mouseOver(square) {
-    console.log('cell');
-    document.getElementsByClassName(cell).style.backgroundColor = 'black';
+function ask() {
+    let input = prompt('Pick a number lower than 100 and bigger than 2: ');
+    
+    if (input >= 2 && input <= 100) {
+        input = parseInt(input);
+        createGrid(input);
+    } else {
+        console.log('error'); 
+    }
+        
 }
 
-document.body.onload = createGrid(256);
 
-
-/*function ask() {
-    let input = prompt('Pick a number lower than 100: ')
-    while (input == null) {
-        input = prompt('Come on, pick a number lower than 100: ')
-    }
-    input = parseInt(input);
-    createGrid(input);
-}*/
+document.body.onload = createGrid(16);
